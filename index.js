14f6fbbb6ac7bc35cd97393493b4df617adb24c7
Nathan Rhead Cox <nrc4@hotmail.com>
2020-12-07 20:08:14 -0800

    Uncommented everything in the index.js.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+// 'use strict';
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const server = require('./server');
+// const server = require('./server');
 
-server.start(3001);
+// server.start(3001);
